10e64a6c9d6b9a54d5473acdd0e695468e38630f
davidburns <burns2dd@mail.uc.edu>
2022-09-22 15:20:37 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.lang.reflect.GenericDeclaration;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        String partyLetter = "d";
+
+        if (partyLetter = "d")
+
+        {
+            System.out.println("You get a Democratic Donkey!");
+        }
+
+        else if (partyLetter = "r")
+        {
+            System.out.println("You get a republican elephant!");
+        }
+
+        else if (partyLetter = "i")
+        {
+            System.out
+        }
+        else
+        {
+            System.out 
+        }
+
+
+    }
+}
